644532f6795d6c8c784d35b464eb3f61cd11c546
NotEvilbook <116756933+Evilbookmainten@users.noreply.github.com>
2024-06-18 11:32:17 -0500

    im tired of getting peaceful, so i made it harder

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 loops.forever(function () {
     loops.pause(60000)
     random2 += randint(1, 40)
-    if (random2 <= 30) {
-        gameplay.title(mobs.target(NEAREST_PLAYER), "§eDifficulty:", "Hard")
+    if (random2 >= 10) {
+        gameplay.title(mobs.target(NEAREST_PLAYER), "§eDifficulty:", "§l§3Hard")
         music.playSound(Sound.LightningImpact)
         gameplay.setDifficulty(HARD)
-    } else if (random2 <= 20) {
+    } else if (random2 >= 20) {
         gameplay.title(mobs.target(NEAREST_PLAYER), "§eDifficulty:", "Normal")
         music.playSound(Sound.Creeper)
         gameplay.setDifficulty(NORMAL)
-    } else if (random2 <= 10) {
+    } else if (random2 >= 30) {
         gameplay.title(mobs.target(NEAREST_PLAYER), "§eDifficulty:", "Easy")
         music.playSound(Sound.Zombie)
         gameplay.setDifficulty(EASY)
@@ -22,4 +22,4 @@ loops.forever(function () {
 let random2 = 0
 player.say("§eRandom Difficulty Changed every Minute!")
 player.say("By §l§eNotEvilbook")
-player.say("Version 1 (Release)")
+player.say("Version 1.1")
